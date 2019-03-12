ca4ec5a4a439c17b9b559fb69a3d1cd4dd7c5964
Ethan Freire <element55star@gmail.com>
2019-03-12 12:00:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key] : value })
 
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value ){
+  object[key]=value
+  return object
+}
+function deleteFromObjectByKey(object,key,value){
+  
+}
+function deleteFromObjectByKey(object, key, value ){
+  
+}
+function destructivelyDeleteFromObjectByKey(object,key){
+  
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
